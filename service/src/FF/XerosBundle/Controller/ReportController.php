5fda82fff7c596e757094d790d078f77d8b42a3e
Jason Yarrington <jason@ffinno.com>
2014-01-23 11:23:45 -0500

    Added drushrc file

--- service/src/FF/XerosBundle/Controller/ReportController.php
+++ service/src/FF/XerosBundle/Controller/ReportController.php
@@ -20,17 +20,81 @@ class ReportController extends Controller {
     /**
      * @return array
      * @View()
-     * @Route("/report/{id}/{start}/{end}.{_format}")
+     * @Route("/report/{uid}/{id}/{fromDate}/{toDate}.{_format}")
      */
-    public function reportAction($id, $start, $end)
+    public function reportAction($uid, $id, $fromDate, $toDate)
     {
-        var_dump("input params", $id, $start, $end);
+        //var_dump("input params", $id, $start, $end);
 
-        $sql = sprintf("SELECT * FROM User WHERE id = '%s'", $id);
-        $conn = $this->get('database_connection');
-        $users = $conn->fetchAll($sql);
+        // {id} = Report ID
 
-        return array ("users" => $users);
+        switch ($id) {
+            case 1:
+                $sql = sprintf("SELECT * FROM users WHERE uid = '%s'", $uid);
+                $conn = $this->get('database_connection');
+                $users = $conn->fetchAll($sql);
+
+                return array ("report" => $users);
+                break;
+            case 2:
+
+                return array("report" => "Report 2");
+                break;
+            case 3:
+
+
+                $json = <<<EOT
+    kpis: [
+        {
+            title: "Overall Cost",
+            unit: "",
+            actualDollars: "6,816",
+            potentialDollars: "3,408",
+            percent: "50",
+            cssClass: "overall",
+            icon: "Globe"
+        },
+        {
+            title: "Cold Water",
+            unit: "Gallons",
+            actualDollars: "1,234",
+            potentialDollars: "619",
+            percent: "50",
+            cssClass: "gallons",
+            icon: "Drop"
+        },
+        {
+            title: "Hot Water",
+            unit: "Efficiency",
+            actualDollars: "731",
+            potentialDollars: "300",
+            percent: "63",
+            cssClass: "efficiency",
+            icon: "Thermometer"
+        },
+        {
+            title: "Cycle Time",
+            unit: "Labor",
+            actualDollars: "63",
+            potentialDollars: "38",
+            percent: "34",
+            cssClass: "labor",
+            icon: "Clock"
+        },
+        {
+            title: "Chemicals",
+            unit: "Usage",
+            actualDollars: "637",
+            potentialDollars: "318",
+            percent: "45",
+            cssClass: "chemicals",
+            icon: "Atom"
+        },
+    ]
+EOT;
+                return array("report" => json_decode($json));
+
+                break;
+        }
     }
-
 } 
