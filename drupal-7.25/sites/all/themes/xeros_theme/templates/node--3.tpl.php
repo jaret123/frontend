2e4909188f1cc32e1b0f03504497480f77fd81b1
Jason Yarrington <jason@ffinno.com>
2014-01-31 00:28:11 -0500

    Updated URLs for web services

--- drupal-7.25/sites/all/themes/xeros_theme/templates/node--3.tpl.php
+++ drupal-7.25/sites/all/themes/xeros_theme/templates/node--3.tpl.php
@@ -12,7 +12,7 @@
     var reports = [
             {
                 rid : 1,
-                apiUrl : 'http://xeros.local/api/report/consumption/2013-11-10/2013-12-20.json',
+                apiUrl : '/api/report/consumption/2013-11-10/2013-12-20.json',
                 template : "consumption",
                 callback : "consumptionCallback"
             }
