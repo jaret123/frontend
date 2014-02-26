c258dfcf97b6e528970b65d6b5d56818c661c01b
Jason Yarrington <jason@ffinno.com>
2014-02-26 12:31:56 -0500

    PDF and CSV functionality working

---
+++ drupal-7.25/pdf/SvgToPdf/ex.php
@@ -0,0 +1,11 @@
+<?php
+
+require('SvgToPDF.php');
+
+//Print 15 labels
+$label=array();
+for($i=1;$i<=15;$i++)
+	$label[]=array('name'=>"Name $i", 'address'=>"Address $i", 'city'=>"City $i");
+$pdf=XML_SvgToPDF::construct('ex.svg', array('label'=>$label));
+$pdf->Output();
+
---
+++ drupal-7.25/pdf/tfpdf/font/unifont/lato-hai.mtx.php
@@ -0,0 +1,19 @@
+<?php
+$name='Lato-Hairline';
+$type='TTF';
+$desc=array (
+  'Ascent' => 987,
+  'Descent' => -213,
+  'CapHeight' => 700,
+  'Flags' => 4,
+  'FontBBox' => '[-107 -190 1059 880]',
+  'ItalicAngle' => 0,
+  'StemV' => 64,
+  'MissingWidth' => 497,
+);
+$up=-95;
+$ut=11;
+$ttffile='/Users/jason/dev/ELYXOR/xeros/drupal-7.25/pdf/tfpdf/font/unifont/Lato-Hai.ttf';
+$originalsize=115316;
+$fontkey='lato';
+?>
