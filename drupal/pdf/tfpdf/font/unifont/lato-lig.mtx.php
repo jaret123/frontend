0e6f8df2b6eba9a6dd3eb3823bc80f84e2b27983
Jason Yarrington <jason@ffinno.com>
2014-03-26 12:25:54 -0400

    Added dynamic news service to the PDF

---
+++ drupal/pdf/tfpdf/font/unifont/lato-lig.mtx.php
@@ -0,0 +1,19 @@
+<?php
+$name='Lato-Light';
+$type='TTF';
+$desc=array (
+  'Ascent' => 987,
+  'Descent' => -213,
+  'CapHeight' => 708,
+  'Flags' => 4,
+  'FontBBox' => '[-101 -186 1085 895]',
+  'ItalicAngle' => 0,
+  'StemV' => 71,
+  'MissingWidth' => 513,
+);
+$up=-83;
+$ut=34;
+$ttffile='/Users/jason/dev/ELYXOR/xeros/drupal/pdf/tfpdf/font/unifont/Lato-Lig.ttf';
+$originalsize=122524;
+$fontkey='lato';
+?>
