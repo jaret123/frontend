3e164fb07ec5c3e301fab83390f711cfc0749434
Jason Yarrington <jason@ffinno.com>
2014-05-05 09:21:47 -0400

    Refactored chemicals to be on page other than classifications

---
+++ drupal/sites/all/modules/custom/xeros_admin/db/tests/machine_classificationTest.php
@@ -0,0 +1,18 @@
+<?php
+
+require_once '../machine_classification.inc';
+
+class machine_classificationTest extends PHPUnit_Framework_TestCase
+{
+  public function setup() { }
+  public function tearDown() {}
+
+  /**
+   * Test simple delete, no children
+   */
+  public function testDelete() {
+
+
+  }
+
+}
