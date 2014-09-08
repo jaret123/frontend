d470dc2b2ba6e9d8da6bd56a4405a345241551ab
Jason Yarrington <jason@ffinno.com>
2014-09-07 23:00:28 -0400

    services

--- drupal/sites/all/modules/custom/xeros_ops/js/ops.js
+++ drupal/sites/all/modules/custom/xeros_ops/js/ops.js
@@ -340,6 +340,19 @@ FF.Hud = (function($){
      */
     pub.init = function() {
 
+        //  format an ISO date using Moment.js
+//  http://momentjs.com/
+//  moment syntax example: moment(Date("2011-07-18T15:50:52")).format("MMMM YYYY")
+//  usage: {{dateFormat creation_date format="MMMM YYYY"}}
+        Handlebars.registerHelper('dateFormat', function(context, block) {
+            if (window.moment) {
+                var f = block.hash.format || "MMM Do, YYYY";
+                return moment(Date(context)).format(f);
+            }else{
+                return context;   //  moment plugin not available. return data as is.
+            };
+        });
+
         load = 0;
         // Reference the window object
         win = $(window);
@@ -376,18 +389,7 @@ var windowStatus = {};
 
 var windowHistory = {};
 
-//  format an ISO date using Moment.js
-//  http://momentjs.com/
-//  moment syntax example: moment(Date("2011-07-18T15:50:52")).format("MMMM YYYY")
-//  usage: {{dateFormat creation_date format="MMMM YYYY"}}
-Handlebars.registerHelper('dateFormat', function(context, block) {
-    if (window.moment) {
-        var f = block.hash.format || "MMM Do, YYYY";
-        return moment(Date(context)).format(f);
-    }else{
-        return context;   //  moment plugin not available. return data as is.
-    };
-});
+
 
 jQuery(document).ready(FF.Hud.init);
 
--- drupal/sites/all/themes/xeros_theme/js/app.js
+++ drupal/sites/all/themes/xeros_theme/js/app.js
@@ -273,6 +273,19 @@ var app = {
             return ( water_only === "1" || water_only === 1 );
         });
 
+        //  format an ISO date using Moment.js
+//  http://momentjs.com/
+//  moment syntax example: moment(Date("2011-07-18T15:50:52")).format("MMMM YYYY")
+//  usage: {{dateFormat creation_date format="MMMM YYYY"}}
+        Handlebars.registerHelper('dateFormat', function(context, block) {
+            if (window.moment) {
+                var f = block.hash.format || "MMM Do, YYYY";
+                return moment(Date(context)).format(f);
+            }else{
+                return context;   //  moment plugin not available. return data as is.
+            };
+        });
+
         self.tpl = Handlebars.compile(jQuery("#page-tpl").html());
 
         self.createDateRanges();
