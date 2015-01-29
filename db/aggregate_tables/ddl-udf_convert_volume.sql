132793f6e91089fb81cb8f4f999ba1e00c3c4bbc
Jason Yarrington <jason@fastforward.sh>
2015-01-29 12:37:42 -0500

    Standardize the data coming in from the meters and from location profile

---
+++ db/aggregate_tables/ddl-udf_convert_volume.sql
@@ -0,0 +1,26 @@
+DELIMITER ;;
+
+DROP FUNCTION IF EXISTS udf_convert_volume;;
+
+CREATE FUNCTION udf_convert_volume( _volume float(10,2), _unit_from varchar(10), _unit_to varchar(10) )
+  RETURNS DECIMAL(10,4)
+  BEGIN
+
+    IF _unit_from = 'liters' and _unit_to = 'gallons' then
+      return _volume * 0.264172;
+    elseif _unit_from = 'gallons' and _unit_to = 'liters' THEN
+      return _volume * 3.78541;
+    elseif _unit_from = _unit_to THEN
+      return _volume;
+    ELSE
+      SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'Invalid parameter passed';
+    end if;
+
+  END ;;
+
+select udf_convert_volume(10, 'gallons', 'liters');;
+select udf_convert_volume(10, 'liters', 'gallons');;
+select udf_convert_volume(10, 'gallons', 'gallons');;
+
+
+select udf_convert_volume(10, 'microns', 'gallons');
