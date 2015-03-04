2d43f0588a340b95f3c2c5ba29fe9d30bf9a5354
Jason Yarrington <jason@fastforward.sh>
2015-03-04 12:13:59 -0500

    Fix bug in sql release script

--- db/updates/db-update-2015-02-25_add_currency_exchange_rate_db_table_triggers.sql
+++ db/updates/db-update-2015-02-25_add_currency_exchange_rate_db_table_triggers.sql
@@ -12,8 +12,10 @@ CREATE TABLE IF NOT EXISTS currency_exchange_rate_db_table_revision (
 /**
   Create triggers to add records to the historical table on create and update
  */
-DROP TRIGGER IF EXISTS currency_exchange_create_trigger
+DROP TRIGGER IF EXISTS currency_exchange_create_trigger;
+
 CREATE TRIGGER currency_exchange_create_trigger AFTER INSERT ON currency_exchange_rate_db_table FOR EACH ROW insert into currency_exchange_rate_db_table_revision (currency_code_from,currency_code_to,rate) values (new.currency_code_from, new.currency_code_to,new.rate);
 
-DROP TRIGGER IF EXISTS currency_exchange_update_trigger
+DROP TRIGGER IF EXISTS currency_exchange_update_trigger;
+
 CREATE TRIGGER currency_exchange_update_trigger AFTER UPDATE ON currency_exchange_rate_db_table FOR EACH ROW insert into currency_exchange_rate_db_table_revision (currency_code_from,currency_code_to,rate) values (new.currency_code_from, new.currency_code_to,new.rate);
